1ddc25c30ccc9efb4e8252c514d2b3aa891de934
Hilful <hilful25@gmail.com>
2022-02-14 02:15:14 +0600

    html file added for graphical interface of the calculator web application

--- calculator.js
+++ calculator.js
@@ -3,11 +3,11 @@ const app = express();
 
 
 app.get('/' , (req , res)=>{
-    res.send("Hello World");
-})
+    res.sendFile(__dirname + "/index.html"); //__dirname is same as pwd
+});
 
 
 
 app.listen('3000',()=>{
     console.log("Listening at port : 3000");
-})
+});
