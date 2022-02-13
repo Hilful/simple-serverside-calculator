dd8ad9599f5decec7a6007ac626a91aeceee6a8a
Hilful <hilful25@gmail.com>
2022-02-14 03:12:00 +0600

    Introduced BMI Calculator @/bmiCalculator

--- calculator.js
+++ calculator.js
@@ -3,9 +3,25 @@ const app = express();
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended:true}));
+
+
+app.get('/bmiCalculator' , (req , res)=>{
+    res.sendFile(__dirname + "/bmiCalculator.html"); //__dirname is same as pwd
+});
+
+app.post("/bmiCalculator", (req,res)=>{
+    var weight = parseFloat(req.body.weight);
+    var height = parseFloat(req.body.height);
+    var bmi = weight / (height*height);
+    console.log(weight);
+    console.log(height);
+    res.send("Your BMI is : " + bmi);
+});
 app.get('/' , (req , res)=>{
     res.sendFile(__dirname + "/index.html"); //__dirname is same as pwd
 });
+
+
 
 app.post("/", (req,res)=>{
     var num1 = Number(req.body.number1);
@@ -15,6 +31,7 @@ app.post("/", (req,res)=>{
     console.log(num2);
     res.send("The result of the sum of two number is " + result);
 })
+
 
 app.listen('3000',()=>{
     console.log("Listening at port : 3000");
